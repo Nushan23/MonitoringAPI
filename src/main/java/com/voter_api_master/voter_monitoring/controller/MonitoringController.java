7100a69c4326d8be88b72bcfbee275f3e11d34f1
Nushan23 <45916993+Nushan23@users.noreply.github.com>
2023-01-14 19:03:32 +0530

    final commit

--- src/main/java/com/voter_api_master/voter_monitoring/controller/MonitoringController.java
+++ src/main/java/com/voter_api_master/voter_monitoring/controller/MonitoringController.java
@@ -19,17 +19,17 @@ public class MonitoringController {
     @Autowired
     RestTemplate restTemplate;
 
-    @RequestMapping(value = "monitorVoter/{id}/{url}/{url2}")
-    public String monitorVoter(@PathVariable("id") Long id, @PathVariable("url") String url, @PathVariable("url2") String url2) {
+    @RequestMapping(value = "monitorVoter/{id}")
+    public String monitorVoter(@PathVariable("id") Long id) {
         String catalog_data = "";
         String location_data = "";
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<String>(headers);
-        String catalog_url = String.format("http://%s:8089/getCatalogData/%x", url, id);
-        String location_url = String.format("http://%s:8087/getLocationDataById/%x", url2, id);
-        catalog_data = restTemplate.exchange(catalog_url, HttpMethod.GET, entity, String.class).getBody();
-        location_data = restTemplate.exchange(location_url, HttpMethod.GET, entity, String.class).getBody();
+        String register_url = String.format("http://localhost:8089/getRegisterDataById/%x", id);
+        String voter_url = String.format("http://localhost:8088/getVoterData/%x", id);
+        catalog_data = restTemplate.exchange(register_url, HttpMethod.GET, entity, String.class).getBody();
+        location_data = restTemplate.exchange(voter_url, HttpMethod.GET, entity, String.class).getBody();
         return (catalog_data + " " + location_data);
     }
 }
